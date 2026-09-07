c3b026d96b871321ee353801f85496d5ccb95b81
kfafa2018-ux <kfafa2018@gmail.com>
2026-09-06 20:19:03 -0600

    Add files via upload
    C++ Hello World 10 facts about myself.

---
+++ Hello COSC 1030!/Hello COSC 1030!.cpp
@@ -0,0 +1,28 @@
+//Kimberley Bennett
+// COSC 1030
+// Hello World!
+#include <iostream>
+using namespace std;
+/*Please write a C++ program that tells us 10  things about yourself:
+
+    Hello to COSC 1030 Course
+    Your Full Name
+    Your first-year student in your major.
+    Hobbies
+    Movies you like 
+    etc..
+*/
+int main() {
+    cout << "Hello, COSC 1030!" << endl;
+    cout << "1.  "  << "My Full Name is Kimberley Bennett" << endl;
+    cout << "2.  "  << "I am in my second year at LCCC for the AI major." << endl;
+    cout << "3.  "  << "My hobbies include : pilates, crafts, coloring, culture, and art." << endl;
+    cout << "4.  "  << "I enjoy supernatural horror, and foreign films." << endl;
+    cout << "5.  "  << "My favorite color is green." << endl;
+    cout << "6.  "  << "I have a cat who is turning 6 near the end of the month." << endl;
+    cout << "7.  "  << "I can speak, read and understand Korean at a beginner-intermediate level." << endl;
+    cout << "8.  "  << "I lived in Japan as a child." << endl;
+    cout << "9.  "  << "I am originally from Massachusetts and deeply miss the Boston area." << endl;
+    cout << "10. " << "I am a women veteran." << endl;
+    return 0;
+}
